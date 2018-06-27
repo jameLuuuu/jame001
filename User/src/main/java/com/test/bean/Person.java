b3a5577120188fcbfbe2c353c7a9cc7c531a0c57
871591918@qq.com <871591918@qq.com>
2018-06-27 16:10:24 +0800

    init files

---
+++ User/src/main/java/com/test/bean/Person.java
@@ -0,0 +1,47 @@
+package com.test.bean;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String address;
+
+    private String name;
+
+    private Integer id;
+
+    private String phone;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
---
+++ User/src/main/java/com/test/mapper/PersonMapper.java
@@ -0,0 +1,22 @@
+package com.test.mapper;
+
+import com.test.bean.Person;
+import com.test.bean.PersonExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PersonMapper {
+    long countByExample(PersonExample example);
+
+    int deleteByExample(PersonExample example);
+
+    int insert(Person record);
+
+    int insertSelective(Person record);
+
+    List<Person> selectByExample(PersonExample example);
+
+    int updateByExampleSelective(@Param("record") Person record, @Param("example") PersonExample example);
+
+    int updateByExample(@Param("record") Person record, @Param("example") PersonExample example);
+}
